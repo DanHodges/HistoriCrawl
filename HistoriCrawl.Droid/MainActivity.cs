e55d324abbeb57e92493b266544dfe110053cdef
DanHodges <dhdgs690@gmail.com>
2015-10-20 17:15:55 -0500

    added setup files for historicrawl

---
+++ HistoriCrawl.Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using HistoriCrawl.Portable;
+
+namespace HistoriCrawl.Droid
+{
+	[Activity(Label = "HistoriCrawl", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
+
+			// Get a handle on the list
+			var markerList = FindViewById<ListView>(Resource.Id.markerListView);
+
+			// Set the list adapter
+			markerList.Adapter = new MarkerAdapter(this, MarkerData.Markers);
+
+			// Set the list ItemClick handler
+			markerList.ItemClick += OnItemClick;
+		}
+
+
+		void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		{
+			var marker = MarkerData.Markers[e.Position];
+
+			var dialog = new AlertDialog.Builder(this);
+			dialog.SetMessage(marker.Title);
+			dialog.SetNeutralButton("OK", delegate { });
+			dialog.Show();
+		}
+	}
+}
